84acf37467a866efb1d3067ca3dfe0eb09adc067
Werkelmann <al-kern@web.de>
2016-08-23 14:38:17 +0200

    ui stubs in activity

--- app/src/main/java/de/werkelmann/rekote/MainActivity.java
+++ app/src/main/java/de/werkelmann/rekote/MainActivity.java
@@ -8,8 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import de.werkelmann.rekote.client.RekoteHttpClient;
+import de.werkelmann.rekote.server.model.HostInfo;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RekoteHttpClient httpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,31 +25,60 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        httpClient = new RekoteHttpClient();
+
+        initButtons();
+    }
+
+    private void initButtons() {
+        Button btnShutdown = (Button) findViewById(R.id.btn_shutdown);
+        Button btnShutdownIn = (Button) findViewById(R.id.btn_shutdown_in);
+        Button btnHostInfo = (Button) findViewById(R.id.btn_host_info);
+
+        assert btnShutdown != null;
+        btnShutdown.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                if (httpClient.shutdown()) {
+                    Toast.makeText(MainActivity.this, "Shutdown successfull", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                    Toast.makeText(MainActivity.this, "Failure at shutdown", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        assert btnShutdownIn != null;
+        btnShutdownIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Shutdown in dialog
+            }
+        });
+
+        assert btnHostInfo != null;
+        btnHostInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HostInfo info = httpClient.getHostInfo();
+                showInfo(info);
             }
         });
     }
 
+    private void showInfo(HostInfo info) {
+        //TODO show infos somehow
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
---
+++ app/src/main/java/de/werkelmann/rekote/client/RekoteHttpClient.java
@@ -0,0 +1,14 @@
+package de.werkelmann.rekote.client;
+
+import de.werkelmann.rekote.server.model.HostInfo;
+
+public class RekoteHttpClient {
+
+    public boolean shutdown() {
+        return false;
+    }
+
+    public HostInfo getHostInfo() {
+        return null;
+    }
+}
